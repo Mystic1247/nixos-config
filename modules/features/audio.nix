fe1e40cd7ea38625039ab840e6eab41f9c7dbd31
Mytic <mysticsoulseeker2@gmail.com>
2026-01-13 17:35:55 +0800

    .

--- modules/features/audio.nix
+++ modules/features/audio.nix
@@ -6,7 +6,17 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # ]\
-    wireplumber.enable = true;
+    wireplumber = {
+      enable = true;
+      # TODO: temp fix
+      extraConfig."10-bluez-fix" = {
+        "monitor.bluez.properties" = {
+          "bluez5.roles" = [ "a2dp_sink" "a2dp_source" ];
+        };
+        "wireplumber.settings" = {
+          "bluetooth.autoswitch-to-headset-profile" = false;
+        };
+      };
+    };
   };
 }
