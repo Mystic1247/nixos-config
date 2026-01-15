8828d4257fb37718c43ec3ab2ed9539aa87abd82
Mytic <mysticsoulseeker2@gmail.com>
2026-01-15 21:18:37 +0800

    .

--- home/mystic/core/default.nix
+++ home/mystic/core/default.nix
@@ -5,4 +5,17 @@
     ./git.nix
     ./zsh.nix
   ];
+
+  programs.ssh = {
+    enable = true;
+    extraConfig = ''
+      AddKeysToAgent yes
+    '';
+    matchBlocks = {
+      "github.com" = {
+        hostname = "github.com";
+        identityFile = "~/.ssh/id_ed25519";
+      };
+    };
+  };
 }
