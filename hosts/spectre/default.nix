3a78559428dbf4acd2dc4979a8e53f84c0087ccb
Mytic <mysticsoulseeker2@gmail.com>
2026-01-16 04:38:51 +0800

    ]

--- hosts/spectre/default.nix
+++ hosts/spectre/default.nix
@@ -14,6 +14,15 @@
   networking.hostName = "spectre";
   time.timeZone = "Asia/Singapore";
 
+  services.logind = {
+  lidSwitch = "ignore";
+  lidSwitchExternalPower = "ignore";
+  lidSwitchDocked = "ignore";
+  extraConfig = ''
+    HandlePowerKey=suspend
+  '';
+};
+
   # This is the "Birth Date" of the install. 
   # It should generally stay what it was when nixos was first installed.
   system.stateVersion = "25.05"; 
--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -1,8 +1,22 @@
 { config, lib, pkgs, ... }:
 
 {
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nixpkgs.config.allowUnfree = true;
-  # ]\
   hardware.enableAllFirmware = true;
-  }
+
+  nix = {
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [ "nix-command" "flakes" ];
+    };
+    
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+
+    registry.nixpkgs.flake = pkgs.path;
+    nixPath = [ "nixpkgs=${pkgs.path}" ];
+  };
+}
